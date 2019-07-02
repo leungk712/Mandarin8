abd60e9a2101bfe617694af9aba2c7df0e9feee7
Kevin Leung <42705941+leungk712@users.noreply.github.com>
2019-07-02 16:49:33 -0600

    Add toggle-content-active class

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const welcome = document.querySelector('.welcome');
 const header = document.querySelector('header');
+
 const aboutBtn = document.querySelector('#about-btn');
 const menuBtn = document.querySelector('#menu-btn');
 const reservationBtn = document.querySelector('#reservation-btn');
 const contactBtn = document.querySelector('#contact-btn');
+
 const aboutContent = document.querySelector('.about-row');
 const menuContent = document.querySelector('.menu-row');
 const reservationContent = document.querySelector('.reservation-row');
@@ -22,20 +24,20 @@ function headerLoad(){
 
 function about(){
   aboutBtn.addEventListener('click',() => {
-    setTimeout(function(){
-      aboutContent.classList.toggle('about-active');
+    setTimeout(() => {
+      aboutContent.classList.toggle('toggle-content-active');
     },500);
   });
 } about();
 
 function menu(){
   menuBtn.addEventListener('click',() => {
-    menuContent.classList.toggle('menu-active');
+    menuContent.classList.toggle('toggle-content-active');
   });
 } menu();
 
 function reservation(){
   reservationBtn.addEventListener('click',() => {
-    reservationContent.classList.toggle('reservation-active');
+    reservationContent.classList.toggle('toggle-content-active');
   });
 } reservation();
